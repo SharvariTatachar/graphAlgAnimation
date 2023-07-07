88dfeb6e1809c59aa047c162d453af9cc94ea5fa
Sharvari Tatachar <statach1@swarthmore.edu>
2023-07-07 13:12:04 -0400

    created nodes.

--- static/js/main.js
+++ static/js/main.js
@@ -12,13 +12,9 @@ function handleTopNavigation(){
     }
 }
 
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-  width = 400 - margin.left - margin.right,
-  height = 400 - margin.top - margin.bottom;
 
-//creating graph for dfs from json file: 
-import data from './graph.json' assert { type: 'json' }; 
-console.log(data)
+
+
 
 
 
